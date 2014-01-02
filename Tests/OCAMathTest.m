2b237f255b301183565c7e8dbd528f6292b2a9e2
Martin Kiss <martin.kiss@me.com>
2014-01-02 16:36:32 +0100

    Add empty Math tests

---
+++ Tests/OCAMathTest.m
@@ -0,0 +1,37 @@
+//
+//  OCAMathTest.m
+//  Objective-Chain
+//
+//  Created by Martin Kiss on 2.1.14.
+//  Copyright (c) 2014 Martin Kiss. All rights reserved.
+//
+
+#import "OCAMath.h"
+
+
+
+@interface OCAMathTest : XCTestCase
+
+@end
+
+
+
+
+
+@implementation OCAMathTest
+
+
+
+
+
+- (void)test {
+    
+}
+
+
+
+
+
+@end
+
+
