12f830420c2160c434236b32649381a0aebc32a4
Martin Kiss <martin.kiss@me.com>
2014-01-10 21:05:25 +0100

    Refactor Foundation Attributed String transformers

--- Sources/Foundation/OCAFoundation+Strings.m
+++ Sources/Foundation/OCAFoundation+Strings.m
@@ -27,12 +27,15 @@
 
 
 + (OCATransformer *)stringWithAttributes:(NSDictionary *)attributes {
-    return [[OCATransformer fromClass:[NSString class] toClass:[NSAttributedString class] transform:^NSAttributedString *(NSString *input) {
-        if ( ! input) return nil;
-        return [[NSAttributedString alloc] initWithString:input attributes:attributes];
-    } reverse:^NSString *(NSAttributedString *input) {
-        return input.string;
-    }]
+    return [[OCATransformer fromClass:[NSString class] toClass:[NSAttributedString class]
+                            transform:^NSAttributedString *(NSString *input) {
+                                
+                                return [[NSAttributedString alloc] initWithString:input attributes:attributes];
+                                
+                            } reverse:^NSString *(NSAttributedString *input) {
+                                
+                                return input.string;
+                            }]
             describe:[NSString stringWithFormat:@"attributed with %@", attributes]];
 }
 
@@ -44,53 +47,56 @@
 
 
 + (OCATransformer *)addAttributes:(NSDictionary *)attributes {
-    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class] symetric:^NSAttributedString *(NSAttributedString *input) {
-        NSMutableAttributedString *mutable = [input mutableCopy];
-        [mutable addAttributes:attributes range:NSMakeRange(0, mutable.length)];
-        return mutable;
-    }]
+    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class]
+                             symetric:^NSAttributedString *(NSAttributedString *input) {
+                                 
+                                 NSMutableAttributedString *mutable = [input mutableCopy];
+                                 [mutable addAttributes:attributes range:NSMakeRange(0, mutable.length)];
+                                 return mutable;
+                             }]
             describe:[NSString stringWithFormat:@"add attributes %@", attributes]];
 }
 
 
 + (OCATransformer *)addAttributes:(NSDictionary *)attributes range:(NSRange)range {
-    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class] symetric:^NSAttributedString *(NSAttributedString *input) {
-        NSMutableAttributedString *mutable = [input mutableCopy];
-        
-        NSUInteger location = CLAMP(0, range.location, mutable.length);
-        NSUInteger length = CLAMP(0, range.length, mutable.length - location);
-        
-        [mutable addAttributes:attributes range:NSMakeRange(location, length)];
-        return mutable;
-    }]
+    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class]
+                             symetric:^NSAttributedString *(NSAttributedString *input) {
+                                 
+                                 NSMutableAttributedString *mutable = [input mutableCopy];
+                                 [mutable addAttributes:attributes range:OCANormalizeRange(range, mutable.length)];
+                                 return mutable;
+                             }]
             describe:[NSString stringWithFormat:@"add attributes %@ at %@", attributes, NSStringFromRange(range)]];
 }
 
 
++ (NSAttributedString *)transformAttributedString:(NSAttributedString *)string attribute:(NSString *)attribute transformer:(NSValueTransformer *)transformer {
+    NSMutableAttributedString *mutable = [string mutableCopy];
+    [mutable enumerateAttribute:attribute
+                        inRange:NSMakeRange(0, mutable.length)
+                        options:kNilOptions
+                     usingBlock:^(id value, NSRange range, BOOL *stop) {
+                         
+                         id transformedValue = [transformer transformedValue:value];
+                         [mutable addAttribute:attribute value:transformedValue range:range];
+                     }];
+    return mutable;
+}
+
+
 + (OCATransformer *)transformAttribute:(NSString *)attribute transformer:(NSValueTransformer *)transformer {
-    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class] transform:^NSAttributedString *(NSAttributedString *input) {
-        NSMutableAttributedString *mutable = [input mutableCopy];
-        [mutable enumerateAttribute:attribute
-                            inRange:NSMakeRange(0, mutable.length)
-                            options:kNilOptions
-                         usingBlock:^(id value, NSRange range, BOOL *stop) {
-                             id transformedValue = [transformer transformedValue:value];
-                             [mutable addAttribute:attribute value:transformedValue range:range];
-                         }];
-        return mutable;
-    } reverse:^NSAttributedString *(NSAttributedString *input) {
-        NSMutableAttributedString *mutable = [input mutableCopy];
-        [mutable enumerateAttribute:attribute
-                            inRange:NSMakeRange(0, mutable.length)
-                            options:kNilOptions
-                         usingBlock:^(id value, NSRange range, BOOL *stop) {
-                             id transformedValue = [transformer reverseTransformedValue:value];
-                             [mutable addAttribute:attribute value:transformedValue range:range];
-                         }];
-        return mutable;
-    }]
+    NSValueTransformer *reversedTransformer = transformer.reversed;
+    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class]
+                            transform:^NSAttributedString *(NSAttributedString *input) {
+                                
+                                return [self transformAttributedString:input attribute:attribute transformer:transformer];
+                                
+                            } reverse:^NSAttributedString *(NSAttributedString *input) {
+                                
+                                return [self transformAttributedString:input attribute:attribute transformer:reversedTransformer];
+                            }]
             describe:[NSString stringWithFormat:@"transform attribute %@ using %@", attribute, transformer]
-            reverse:[NSString stringWithFormat:@"transform attribute %@ using %@", attribute, [transformer reversed]]];
+            reverse:[NSString stringWithFormat:@"transform attribute %@ using %@", attribute, reversedTransformer]];
 }
 
 
@@ -101,36 +107,42 @@
 
 
 + (OCATransformer *)appendAttributedString:(NSAttributedString *)attributedString {
-    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class] transform:^NSAttributedString *(NSAttributedString *input) {
-        NSMutableAttributedString *mutable = [input mutableCopy];
-        [mutable appendAttributedString:attributedString];
-        return mutable;
-    } reverse:OCATransformationPass]
+    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class]
+                            transform:^NSAttributedString *(NSAttributedString *input) {
+                                
+                                NSMutableAttributedString *mutable = [input mutableCopy];
+                                [mutable appendAttributedString:attributedString];
+                                return mutable;
+                                
+                            } reverse:OCATransformationPass]
+            
             describe:[NSString stringWithFormat:@"append %@", attributedString]
             reverse:@"pass"];
 }
 
 
 + (OCATransformer *)attributedSubstringInRange:(NSRange)range {
-    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class] transform:^NSAttributedString *(NSAttributedString *input) {
-        
-        NSUInteger location = CLAMP(0, range.location, input.length);
-        NSUInteger length = CLAMP(0, range.length, input.length - location);
-        
-        return [input attributedSubstringFromRange:NSMakeRange(location, length)];
-    } reverse:OCATransformationPass]
+    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class]
+                            transform:^NSAttributedString *(NSAttributedString *input) {
+                                
+                                return [input attributedSubstringFromRange:OCANormalizeRange(range, input.length)];
+                                
+                            } reverse:OCATransformationPass]
+            
             describe:[NSString stringWithFormat:@"substring %@", NSStringFromRange(range)]
             reverse:@"pass"];
 }
 
 
 + (OCATransformer *)mutateAttributedString:(void(^)(NSMutableAttributedString *attributedString))block {
-    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class] asymetric:^NSAttributedString *(NSAttributedString *input) {
-        NSMutableAttributedString *mutable = [input mutableCopy];
-        block(mutable);
-        return mutable;
-    }]
-            describe:[NSString stringWithFormat:@"mutate attributed string"]];
+    return [[OCATransformer fromClass:[NSAttributedString class] toClass:[NSAttributedString class]
+                            asymetric:^NSAttributedString *(NSAttributedString *input) {
+                                
+                                NSMutableAttributedString *mutable = [input mutableCopy];
+                                block(mutable);
+                                return mutable;
+                            }]
+            describe:@"mutate attributed string"];
 }
 
 
