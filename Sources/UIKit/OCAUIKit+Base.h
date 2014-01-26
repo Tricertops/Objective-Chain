640a42c4524e4d6b6bbe691f90345e6841b2183c
Martin Kiss <martin.kiss@me.com>
2014-01-26 16:39:44 +0100

    Add UIKit predefined Consumers to set UIButton’s state-based properties

--- Sources/UIKit/OCAUIKit+Base.h
+++ Sources/UIKit/OCAUIKit+Base.h
@@ -10,12 +10,26 @@
 
 #import "OCAObject.h"
 #import "OCATransformer.h"
+#import "OCASubscriber.h"
 
 
 
 
 
 @interface OCAUIKit : OCAObject
+
+
+
+#pragma mark UIButton
+
++ (id<OCAConsumer>)setTitleOfButton:(UIButton *)button forControlState:(UIControlState)state;
++ (id<OCAConsumer>)setAttributedTitleOfButton:(UIButton *)button forControlState:(UIControlState)state;
++ (id<OCAConsumer>)setTitleColorOfButton:(UIButton *)button forControlState:(UIControlState)state;
++ (id<OCAConsumer>)setTitleShadowColorOfButton:(UIButton *)button forControlState:(UIControlState)state;
++ (id<OCAConsumer>)setImageOfButton:(UIButton *)button forControlState:(UIControlState)state;
++ (id<OCAConsumer>)setBackgroundImageOfButton:(UIButton *)button forControlState:(UIControlState)state;
+
+
 
 @end
 
--- Sources/UIKit/OCAUIKit+Base.m
+++ Sources/UIKit/OCAUIKit+Base.m
@@ -8,6 +8,64 @@
 
 #import "OCAUIKit+Base.h"
 
+
+
+
+
 @implementation OCAUIKit
 
+
+
+
+
+#pragma mark UIButton
+
+
++ (id<OCAConsumer>)setTitleOfButton:(UIButton *)button forControlState:(UIControlState)state {
+    return [OCASubscriber class:[NSString class] handler:^(NSString *input) {
+        [button setTitle:input forState:state];
+    }];
+}
+
+
++ (id<OCAConsumer>)setAttributedTitleOfButton:(UIButton *)button forControlState:(UIControlState)state {
+    return [OCASubscriber class:[NSAttributedString class] handler:^(NSAttributedString *input) {
+        [button setAttributedTitle:input forState:state];
+    }];
+}
+
+
++ (id<OCAConsumer>)setTitleColorOfButton:(UIButton *)button forControlState:(UIControlState)state {
+    return [OCASubscriber class:[UIColor class] handler:^(UIColor *input) {
+        [button setTitleColor:input forState:state];
+    }];
+}
+
+
++ (id<OCAConsumer>)setTitleShadowColorOfButton:(UIButton *)button forControlState:(UIControlState)state {
+    return [OCASubscriber class:[UIColor class] handler:^(UIColor *input) {
+        [button setTitleShadowColor:input forState:state];
+    }];
+}
+
+
++ (id<OCAConsumer>)setImageOfButton:(UIButton *)button forControlState:(UIControlState)state {
+    return [OCASubscriber class:[UIImage class] handler:^(UIImage *input) {
+        [button setImage:input forState:state];
+    }];
+}
+
+
++ (id<OCAConsumer>)setBackgroundImageOfButton:(UIButton *)button forControlState:(UIControlState)state {
+    return [OCASubscriber class:[UIImage class] handler:^(UIImage *input) {
+        [button setBackgroundImage:input forState:state];
+    }];
+}
+
+
+
+
+
 @end
+
+
