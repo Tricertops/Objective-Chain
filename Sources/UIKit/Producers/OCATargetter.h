b599e6de96505a0a15d37415322e52819dff4cdb
Martin Kiss <martin.kiss@me.com>
2014-01-28 13:58:20 +0100

    Rename Control Target to Targetter and make it universal

---
+++ Sources/UIKit/Producers/OCATargetter.h
@@ -0,0 +1,66 @@
+//
+//  OCATargetter.h
+//  Objective-Chain
+//
+//  Created by Martin Kiss on 15.1.14.
+//  Copyright (c) 2014 Martin Kiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OCAProducer.h"
+
+
+
+
+
+@interface OCATargetter : OCAProducer
+
+
+
+- (instancetype)initWithOwner:(id)owner;
+
++ (instancetype)targetForControl:(UIControl *)control events:(UIControlEvents)events;
++ (instancetype)targetForBarButtonItem:(UIBarButtonItem *)barButtonItem;
++ (instancetype)targetGestureRecognizer:(UIGestureRecognizer *)gestureRecognizer;
+
+@property (atomic, readonly, weak) id owner;
+
+@property (atomic, readonly, assign) SEL action;
+
+
+
+@end
+
+
+
+
+
+@interface UIControl (OCATargetter)
+
+- (OCATargetter *)producerForEvent:(UIControlEvents)event;
+//TODO: producerForTouchUpInside
+//TODO: producerForValueChanged
+
+@end
+
+
+
+
+
+@interface UIBarButtonItem (OCATargetter)
+
+- (OCATargetter *)producer;
+
+@end
+
+
+
+
+
+@interface UIGestureRecognizer (OCATargetter)
+
+- (OCATargetter *)producer;
+
+@end
+
+
