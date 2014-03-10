0bba7d265be4aac71e3b83f3cfae51e949b60666
Martin Kiss <martin.kiss@me.com>
2014-03-10 13:58:49 +0100

    Examples – Trottled: Create new example to show how to use Throttle

---
+++ Examples/Examples/Throttled/CAEThrottledExample.h
@@ -0,0 +1,13 @@
+//
+//  CAEThrottledExample.h
+//  Chain Examples
+//
+//  Created by Martin Kiss on 10.3.14.
+//  Copyright (c) 2014 iMartin Kiss. All rights reserved.
+//
+
+#import "CAEExampleViewController.h"
+
+@interface CAEThrottledExample : CAEExampleViewController
+
+@end
---
+++ Examples/Examples/Throttled/CAEThrottledExample.m
@@ -0,0 +1,118 @@
+//
+//  CAEThrottledExample.m
+//  Chain Examples
+//
+//  Created by Martin Kiss on 10.3.14.
+//  Copyright (c) 2014 iMartin Kiss. All rights reserved.
+//
+
+#import "CAEThrottledExample.h"
+
+
+
+
+
+@interface CAEThrottledExample ()
+
+
+
+@property (nonatomic, readwrite, strong) UILabel *label;
+@property (nonatomic, readwrite, strong) UISlider *slider;
+
+
+
+@end
+
+
+
+
+
+
+
+
+
+
+@implementation CAEThrottledExample
+
+
+
+
+
+#pragma mark Example Info & Registration
+
+
++ (void)load {
+    [self registerExample];
+}
+
+
++ (NSString *)exampleTitle {
+    return @"Throttled";
+}
+
+
++ (NSString *)exampleSubtitle {
+    return @"Limiting frequency of events";
+}
+
+
++ (NSString *)exampleDescription {
+    return @"";
+}
+
+
++ (NSString *)exampleAuthor {
+    return @"iMartin Kiss";
+}
+
+
++ (NSDate *)exampleDate {
+    return [self day:10 month:3 year:2014];
+}
+
+
+
+
+
+#pragma mark Setup
+
+
+- (void)setupViews {
+    [super setupViews];
+    
+    self.label = ({
+        UILabel *label = [[UILabel alloc] init];
+        label.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:90];
+        label.backgroundColor = [UIColor clearColor];
+        label.frame = CGRectMake(20, 64+40, 280, label.font.pointSize);
+        label.text = @"1000";
+        label.textAlignment = NSTextAlignmentCenter;
+        
+        [self.view addSubview:label];
+        label;
+    });
+    
+    self.slider = ({
+        UISlider *slider = [[UISlider alloc] init];
+        slider.frame = CGRectMake(20, CGRectGetMaxY(self.label.frame) + 60, 280, 44);
+        slider.minimumValue = 0;
+        slider.maximumValue = 1000;
+        
+        [self.view addSubview:slider];
+        slider;
+    });
+    
+}
+
+
+- (void)setupChains {
+    [super setupChains];
+}
+
+
+
+
+
+@end
+
+
